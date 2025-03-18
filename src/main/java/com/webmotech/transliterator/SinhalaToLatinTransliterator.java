d9a57f19ac816777a273ce6725bfa5dcca29d8ff
iroshperera <iroshlashan31@gmail.com>
2025-03-18 20:55:21 +0530

    feat: Add new Sinhala to Latin transliteration rules

--- src/main/java/com/webmotech/transliterator/SinhalaToLatinTransliterator.java
+++ src/main/java/com/webmotech/transliterator/SinhalaToLatinTransliterator.java
@@ -11,18 +11,20 @@ public class SinhalaToLatinTransliterator {
         // Vowels
         SINHALA_TO_LATIN_RULES.put("අ", "a");
         SINHALA_TO_LATIN_RULES.put("ආ", "aa");
-        SINHALA_TO_LATIN_RULES.put("ඇ", "ae");
-        SINHALA_TO_LATIN_RULES.put("ඈ", "aae");
+        SINHALA_TO_LATIN_RULES.put("ඇ", "a");
+        SINHALA_TO_LATIN_RULES.put("ඈ", "aa");
         SINHALA_TO_LATIN_RULES.put("ඉ", "i");
         SINHALA_TO_LATIN_RULES.put("ඊ", "ii");
         SINHALA_TO_LATIN_RULES.put("උ", "u");
         SINHALA_TO_LATIN_RULES.put("ඌ", "uu");
+        SINHALA_TO_LATIN_RULES.put("ඍ", "r");
+        SINHALA_TO_LATIN_RULES.put("ඎ", "ru");
         SINHALA_TO_LATIN_RULES.put("එ", "e");
         SINHALA_TO_LATIN_RULES.put("ඒ", "ee");
+        SINHALA_TO_LATIN_RULES.put("ඓ", "ai");
         SINHALA_TO_LATIN_RULES.put("ඔ", "o");
         SINHALA_TO_LATIN_RULES.put("ඕ", "oo");
-        SINHALA_TO_LATIN_RULES.put("ඖ", "ou");
-        SINHALA_TO_LATIN_RULES.put("඘", "au");
+        SINHALA_TO_LATIN_RULES.put("ඖ", "au");
 
         // Consonants
         SINHALA_TO_LATIN_RULES.put("ක", "ka");
@@ -35,49 +37,61 @@ public class SinhalaToLatinTransliterator {
         SINHALA_TO_LATIN_RULES.put("ජ", "ja");
         SINHALA_TO_LATIN_RULES.put("ඣ", "zha");
         SINHALA_TO_LATIN_RULES.put("ඤ", "nya");
-        SINHALA_TO_LATIN_RULES.put("ට", "ṭa");
+        SINHALA_TO_LATIN_RULES.put("ට", "ta");
         SINHALA_TO_LATIN_RULES.put("ඨ", "ṭha");
         SINHALA_TO_LATIN_RULES.put("ඩ", "da");
         SINHALA_TO_LATIN_RULES.put("ඪ", "dha");
         SINHALA_TO_LATIN_RULES.put("ණ", "na");
         SINHALA_TO_LATIN_RULES.put("ත", "tha");
         SINHALA_TO_LATIN_RULES.put("ථ", "thha");
-        SINHALA_TO_LATIN_RULES.put("ද", "da");
-        SINHALA_TO_LATIN_RULES.put("ධ", "dha");
+        SINHALA_TO_LATIN_RULES.put("ද", "dha");
+        SINHALA_TO_LATIN_RULES.put("ධ", "dhha");
         SINHALA_TO_LATIN_RULES.put("න", "na");
         SINHALA_TO_LATIN_RULES.put("ප", "pa");
         SINHALA_TO_LATIN_RULES.put("ඵ", "pha");
         SINHALA_TO_LATIN_RULES.put("බ", "ba");
         SINHALA_TO_LATIN_RULES.put("භ", "bha");
         SINHALA_TO_LATIN_RULES.put("ම", "ma");
-        SINHALA_TO_LATIN_RULES.put("ඹ", "mba");
+        SINHALA_TO_LATIN_RULES.put("ඹ", "ba");
         SINHALA_TO_LATIN_RULES.put("ය", "ya");
         SINHALA_TO_LATIN_RULES.put("ර", "ra");
         SINHALA_TO_LATIN_RULES.put("ල", "la");
-        SINHALA_TO_LATIN_RULES.put("ළ", "ḷa");
-        SINHALA_TO_LATIN_RULES.put("ව", "va");
+        SINHALA_TO_LATIN_RULES.put("ළ", "La");
+        SINHALA_TO_LATIN_RULES.put("ව", "wa");
         SINHALA_TO_LATIN_RULES.put("ශ", "sha");
-        SINHALA_TO_LATIN_RULES.put("ෂ", "ṣa");
+        SINHALA_TO_LATIN_RULES.put("ෂ", "Sha");
         SINHALA_TO_LATIN_RULES.put("ස", "sa");
         SINHALA_TO_LATIN_RULES.put("හ", "ha");
         SINHALA_TO_LATIN_RULES.put("ෆ", "fa");
 
         // Special characters and sounds
-        SINHALA_TO_LATIN_RULES.put("ං", "ng");
+        SINHALA_TO_LATIN_RULES.put("ං", "kax");
         SINHALA_TO_LATIN_RULES.put("්", "");
         SINHALA_TO_LATIN_RULES.put("්‍", "");
         SINHALA_TO_LATIN_RULES.put("්‍ර", "ra");
-        SINHALA_TO_LATIN_RULES.put("ෲ", "ru");
+        SINHALA_TO_LATIN_RULES.put("ෲ", "ruu");
         SINHALA_TO_LATIN_RULES.put("ඉ්", "i");
         SINHALA_TO_LATIN_RULES.put("උ්", "u");
         SINHALA_TO_LATIN_RULES.put("ෞ", "au");
 
-        // Combining characters
+        // Combining characters (diacritics)
         SINHALA_TO_LATIN_RULES.put("ා", "a");
         SINHALA_TO_LATIN_RULES.put("ෙ", "e");
         SINHALA_TO_LATIN_RULES.put("ෑ", "ae");
         SINHALA_TO_LATIN_RULES.put("ු", "u");
         SINHALA_TO_LATIN_RULES.put("ූ", "uu");
+
+        // Other special mappings
+        SINHALA_TO_LATIN_RULES.put("ක්‍ය", "kya");
+        SINHALA_TO_LATIN_RULES.put("ක්‍ර", "kra");
+        SINHALA_TO_LATIN_RULES.put("ඟ", "zga");
+        SINHALA_TO_LATIN_RULES.put("ඦ", "zja");
+        SINHALA_TO_LATIN_RULES.put("ඬ", "nda");
+        SINHALA_TO_LATIN_RULES.put("ඳ", "ndha");
+        SINHALA_TO_LATIN_RULES.put("ඤ", "nka");
+        SINHALA_TO_LATIN_RULES.put("ඥ", "nha");
+        SINHALA_TO_LATIN_RULES.put("ඹ", "ba");
+        SINHALA_TO_LATIN_RULES.put("ළු", "lu");
 
         // Digits and punctuation marks
         SINHALA_TO_LATIN_RULES.put("0", "0");
